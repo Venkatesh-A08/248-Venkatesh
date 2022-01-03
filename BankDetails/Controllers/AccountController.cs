d6cf431357e48cb014132c52e2643ff6040e6858
Venkatesh A <venkatesh81299@gmail.com>
2022-01-03 14:29:37 +0530

    Exercise ViewData & ViewBag

---
+++ BankDetails/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankDetails.Models;
+
+namespace BankDetails.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Account> Acc = new List<Account>();
+            Acc.Add(new Account() { A_Id = 123, A_Name = "Venkat", A_Address = "abc" });
+            Acc.Add(new Account() { A_Id = 122, A_Name = "Deepak", A_Address = "XYZ" });
+            Acc.Add(new Account() { A_Id = 124, A_Name = "Mugesh", A_Address = "PQR" });
+            Acc.Add(new Account() { A_Id = 125, A_Name = "Prabha", A_Address = "LMN" });
+            Acc.Add(new Account() { A_Id = 127, A_Name = "Vishwa", A_Address = "FGH" });
+
+            ViewData["Account"] = Acc;
+            ViewBag.Total = Acc.Count();
+            return View();
+        }
+    }
+}
---
+++ BankDetails/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankDetails.Models
+{
+    public class Account
+    {
+        public int A_Id { get; set; }
+        public string A_Name { get; set; }
+        public string A_Address { get; set; }
+    }
+}
