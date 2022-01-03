62262301327dc52eea4fc4526f16c71a71bce3e5
Venkatesh A <venkatesh81299@gmail.com>
2022-01-03 14:27:06 +0530

    Practice View Data

---
+++ ViewData/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViewData.Models;
+
+namespace ViewData.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Employee> employees = new List<Employee>();
+            employees.Add(new Employee() { Id = 123, FirstName = "Venkat", LastName = "Deepak" });
+            employees.Add(new Employee() { Id = 1234, FirstName = "Raj", LastName = "Kumar" });
+            employees.Add(new Employee() { Id = 123, FirstName = "Siva", LastName = "Kumar" });
+
+            ViewData["Employee"] = employees;
+            return View();
+        }
+    }
+}
