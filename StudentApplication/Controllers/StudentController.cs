078678d003c43cfd256887d21a66652de1ed6350
Venkatesh A <venkatesh81299@gmail.com>
2022-01-03 14:33:56 +0530

    Practices program

---
+++ StudentApplication/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetStudentId()
+        {
+            return View();
+        }
+        public IActionResult GetStudentName()
+        {
+            return View();
+        }
+    }
+}
