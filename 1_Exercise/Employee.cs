6569e5d8749b80b51f63c98c55797b3d688803f5
Venkatesh A <venkatesh81299@gmail.com>
2022-01-03 14:22:37 +0530

    C# Assignment

---
+++ 1_Exercise/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_Exercise
+{
+    class Employee : Department
+    {
+        public Employee()
+        {
+        }
+        public override string GetDepDetails()
+        {
+            Console.WriteLine("\nAdd Employee Details\n");
+            string x = GetFirstName() + " " + GetLastName();
+            return x;
+        }
+        public string GetFirstName()
+        {
+            Console.WriteLine("\nEnter a First Name : ");
+            string x = Console.ReadLine();
+            return x;
+        }
+        public string GetLastName()
+        {
+            Console.WriteLine("\nEnter a Last Name : ");
+            string x = Console.ReadLine();
+            return x;
+        }
+    }
+}
---
+++ 1_Exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1_Exercise
+{
+    class Program
+    {
+        static void Main(string[]  args)
+        {
+            Department Dep = new Department();
+            Department Emp = new Employee();
+
+            int NumOfDep = Dep.GetNumberOfDep();
+            for (int i = 1; i <= NumOfDep; i++)
+            {
+                Console.WriteLine("\tDepartment Name : {0}\n \tEmployee Name : {1}\n", Dep.GetDepName(), Emp.GetDepDetails());
+            }
+        }
+    }
+}
