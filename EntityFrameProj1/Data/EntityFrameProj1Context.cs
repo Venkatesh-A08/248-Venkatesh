c20ccac0e0a4640887608057dfa13007de335957
Venkatesh A <venkatesh81299@gmail.com>
2022-01-04 18:06:53 +0530

    EntityExample

---
+++ EntityFrameProj1/Data/EntityFrameProj1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntityFrameProj1.Models;
+
+namespace EntityFrameProj1.Data
+{
+    public class EntityFrameProj1Context : DbContext
+    {
+        public EntityFrameProj1Context (DbContextOptions<EntityFrameProj1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EntityFrameProj1.Models.Students> Students { get; set; }
+    }
+}
