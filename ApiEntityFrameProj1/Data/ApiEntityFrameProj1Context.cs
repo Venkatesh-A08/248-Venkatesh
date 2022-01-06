28ae8277844d1b5cba69ef0e7ca868f148df214c
Venkatesh A <venkatesh81299@gmail.com>
2022-01-06 18:01:10 +0530

    Api Entity

---
+++ ApiEntityFrameProj1/Data/ApiEntityFrameProj1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiEntityFrameProj1.Models;
+
+namespace ApiEntityFrameProj1.Data
+{
+    public class ApiEntityFrameProj1Context : DbContext
+    {
+        public ApiEntityFrameProj1Context (DbContextOptions<ApiEntityFrameProj1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiEntityFrameProj1.Models.Employee> Employee { get; set; }
+    }
+}
