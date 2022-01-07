c385974464b3d450d13a31fc8ebaf06b33e7d6f1
Venkatesh A <venkatesh81299@gmail.com>
2022-01-07 17:46:47 +0530

    Exercise

---
+++ ApiEntityDepProj/Data/ApiEntityDepProjContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiEntityDepProj.Models;
+
+namespace ApiEntityDepProj.Data
+{
+    public class ApiEntityDepProjContext : DbContext
+    {
+        public ApiEntityDepProjContext (DbContextOptions<ApiEntityDepProjContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiEntityDepProj.Models.Department> Department { get; set; }
+    }
+}
---
+++ ApiEntityDepProj/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiEntityDepProj.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string DepartmentName { get; set; }
+        public string Course { get; set; }
+        public string Location { get; set; }
+    }
+}
