6b402a1af4260c7ef47588c2d1f486f5dafd25bb
Venkatesh A <venkatesh81299@gmail.com>
2022-01-11 17:08:58 +0530

    Ado.Net Exercise

---
+++ AdoPractiseProj2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdoPractiseProj2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = null;
+
+            con = new SqlConnection(@"data source=(localdb)\MSSQLLocalDB;database=NorthWind; integrated security=SSPI");
+            SqlCommand cm = new SqlCommand("Select * from Employees", con);
+            con.Open();
+            SqlDataReader sdr = cm.ExecuteReader();
+
+            while (sdr.Read())
+            {
+                Console.WriteLine(" | {0,10} | {1,10} |",sdr["FirstName"] ,sdr["City"]);
+            }
+        }
+    }
+}
