fb2da14dc3994ff623590cebae77379063b30d06
Venkatesh A <venkatesh81299@gmail.com>
2022-01-11 18:22:23 +0530

    Data First

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new NorthwindContext())
+            {
+                List<Customers> cus = context.Customers.ToList();
+
+                foreach(var customer in cus)
+                {
+                    Console.WriteLine("| {0,10} | {0,10} |",customer.ContactName,customer.CompanyName);
+                }
+            }
+        }
+    }
+}
