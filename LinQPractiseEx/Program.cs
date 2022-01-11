2a2c01982ae56ceacac05eb085c0e9a7668b0ebe
Venkatesh A <venkatesh81299@gmail.com>
2022-01-11 17:05:52 +0530

    LinQ Exersice

---
+++ LinQPractiseEx/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace LinQPractiseEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] person = {"Venkatesh", "Mark", "Mugesh", "AjayKumar", "Aravind", "Selish",
+                "Aathisivan","SivaKumar" };
+
+            FindNames(person);
+        }
+        public static void FindNames(string[] person)
+        {
+            IEnumerable<string> query = from s in person
+                                        where s.Length > 5 & s.Contains("a")
+                                        orderby s
+                                        select s.ToUpper();
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
