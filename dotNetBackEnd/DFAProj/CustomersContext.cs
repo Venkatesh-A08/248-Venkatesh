7a7d4caf333c37946c8a2c2443cf9d72cc51b6d0
Venkatesh A <venkatesh81299@gmail.com>
2022-01-17 12:44:12 +0530

    Data First Approach Exercise

---
+++ dotNetBackEnd/DFAProj/CustomersContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DFAProj
+{
+    public partial class CustomersContext : DbContext
+    {
+        public CustomersContext()
+        {
+        }
+
+        public CustomersContext(DbContextOptions<CustomersContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customer { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Customers");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.Location)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.PhoneNumber)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ dotNetBackEnd/DFAProj/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace DFAProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new CustomersContext())
+            {
+                List<Customer> det = context.Customer.ToList();
+
+                foreach (var details in det)
+                {
+                    Console.WriteLine("| {0,10} | {1,10} |", details.Name, details.Location);
+                }
+            }
+        }
+    }
+}
